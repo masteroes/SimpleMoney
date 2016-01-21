743193c6ee4a510101223bdf13c97846b46f1326
ecoworld007 <ecoworld007@gmail.com>
2016-01-21 17:27:33 +0530

    account manager

--- account/account.js
+++ account/account.js
@@ -1,15 +1,31 @@
 var express=require('express');
+var mongoose=require('mongoose');
 var app=express();
-
-app.get('/transaction/:userId/credit',function(req,res){
-	
+var AccountModel=require('./accountModel.js');
+mongoose.connect('mongodb://localhost/testdb');
+app.get('/users/:userId/accounts',function(req,res){
+	mongoose.model('account').find({},function(err,posts){
+		res.send(posts);
+	});
 });
 
-app.get('/transaction/:userId/debit',function(req,res){
-	
+app.get('/users/:userId/accounts/:accountID',function(req,res){
+	mongoose.model('account').find({userID : req.params.userId,accountID : req.params.accountID},function(err,posts){
+		res.send(posts);
+	});
 });
-
-app.get('/transactionall',function(req,res){
-	
-})
-
+app.post('/users/:userId/accounts',function(req,res){
+	var newAccount=new AccountModel({
+	userID:req.params.userId,
+	accountID:req.params.userId+'_'+Math.floor(100000 + Math.random() * 900000),
+	type:"Saving",
+	amount:0.0,
+	dateCreated:new Date()
+});
+	newAccount.save(function(err,AccountModel){
+		res.send(AccountModel);
+	})
+});
+app.listen(process.env.PORT||3000,function(){
+	console.log('Example app listening on port 3000!');
+});
