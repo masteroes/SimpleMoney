83d19a96e96c6127ced373d01cd68ce1fc8bb927
ecoworld007@gmail.com <ecoworld007@gmail.com>
2016-01-22 01:35:56 +0530

    fixes for account managing module

--- account/account.js
+++ account/account.js
@@ -1,17 +1,28 @@
 var shortid = require('shortid');
 var express=require('express');
 var mongoose=require('mongoose');
+var bodyParser = require('body-parser');
+var AccountModel=require('./model/accountModel.js');
 var app=express();
-var AccountModel=require('./accountModel.js');
+mongoose.connect('mongodb://localhost/mydb');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //for fetching list of account details for given user
 app.get('/users/:userId/accounts',function(req,res){
-	mongoose.model('account').find({userID : req.params.userId},function(err,posts){
+	mongoose.model('account').find({userID : req.params.userId}, { _id: 0 ,__v: 0 },function(err,posts){
+		if(err){
+			res.send("error in fetching account for user : "+req.params.userId);
+		}
 		res.send(posts);
 	});
 });
 //for fetching account details of given account for a given user
 app.get('/users/:userId/accounts/:accountID',function(req,res){
-	mongoose.model('account').find({userID : req.params.userId,accountID : req.params.accountID},function(err,posts){
+	mongoose.model('account').find({userID : req.params.userId,accountID : req.params.accountID}, { _id: 0 ,__v: 0 },function(err,posts){
+		if(err){
+			res.send("error in fetching account "+req.params.accountID+" for user : "+req.params.userId);
+		}
 		res.send(posts);
 	});
 });
@@ -22,9 +33,17 @@ app.post('/users/:userId/accounts',function(req,res){
 		res.end();
 	}else{
 		req.body.accountID=shortid.generate();
-		var newAccount=new AccountModel(req.body));
+		req.body.dateCreated=new Date();
+		var newAccount=new AccountModel(req.body);
+		console.log(newAccount);
 		newAccount.save(function(err,AccountModel){
+			if(err){
+			res.send("error while creating account for user : "+req.params.userId);
+		}
 			res.send(AccountModel);
 		});
     }
+});
+app.listen(process.env.PORT||3000,function(){
+	console.log('Example app listening on port 3000!');
 });
