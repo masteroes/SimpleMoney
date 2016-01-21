d4abd7a2a7f496d35669aebeeb0fa23abab4a429
ecoworld007 <ecoworld007@gmail.com>
2016-01-21 13:17:31 +0530

    account history

---
+++ account/account.js
@@ -0,0 +1,15 @@
+var express=require('express');
+var app=express();
+
+app.get('/transaction/:userId/credit',function(req,res){
+	
+});
+
+app.get('/transaction/:userId/debit',function(req,res){
+	
+});
+
+app.get('/transactionall',function(req,res){
+	
+})
+
