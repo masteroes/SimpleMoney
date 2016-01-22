5e8e2aceb241b2133757f39e6468db4c7d89c203
ashutoshrnjn <ashutoshranjan.33@gmail.com>
2016-01-22 12:40:53 +0530

    validations

--- server.js
+++ server.js
@@ -3,43 +3,40 @@ var User=require('./userMgmt/models/users.js');
 var app=express();
 var mongoose=require('mongoose');
 var bodyParser = require('body-parser');
+var validator = require("email-validator");
 
-app.use(express.static('public'));
 
 app.use(bodyParser.json());
-
 mongoose.connect('mongodb://localhost:27017/simplemoney');
-
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/users',function(req,res){	
-	console.log("Body Before");
-	console.log("Body is ",req.body.username);	
-	var user = new User({"username":req.body.username,"password":req.body.password,"contactinfo":{"phone":req.body.contactinfo.phone,
-	"email":req.body.contactinfo.email},"addressinfo":{"add1":req.body.addressinfo.add1},
-	"personalinfo":{"name":req.body.personalinfo.name}});
-	console.log('Before Save');
- 	user.save(function(err,users){	
-		if(err)
-			res.send(err);
-		else
-    	res.send(users);
-    });
+app.post('/users',function(req,res){
+	if(!validator.validate(req.body.contactinfo.email)){
+		res.send("Email is not valid");
+	}else{
+		var user = new User(req.body);
+		user.save(function(err,users){
+			if(err)
+				res.send(err);
+			else
+				res.send(users);
+		});
+	}
 });
 
 
-app.get('/users',function(req,res){	
+app.get('/users',function(req,res){
 	mongoose.model('users').find(function(err,users){
 	res.send(users);
 	});
 });
 
-app.get('/users/:username',function(req,resp){	
+app.get('/users/:username',function(req,resp){
 	mongoose.model('users').find({ username: req.params.username },function(err,users){
 	resp.send(users);
-	});	
+	});
 });
 
 app.listen(process.env.APP_PORT || 4000,function(){
-	console.log(process.env.APP_PORT)
+	//console.log(process.env.APP_PORT)
 });
--- userMgmt/models/users.js
+++ userMgmt/models/users.js
@@ -1,19 +1,27 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 var Schema = mongoose.Schema;
 
 var usersSchema = new Schema({
-  username:String,
-  password:String,
+  username:{type:String,required: true, unique: true},
+  password:{type:String,required: true},
   contactinfo:{
     phone : String,
-    email : String
+    email : {type:String,required: true, unique: true}
   },
   addressinfo: {
-    add1: String
+    add1: String,
+    add2: String,
+    city: String,
+    state: String,
+    pin: String
   },
   personalinfo:{
-    name: String
+    name: String,
+    age: Number,
+    sex: String
   }
 });
+usersSchema.plugin(uniqueValidator);
 var users= mongoose.model('users',usersSchema);
 module.exports = users;
