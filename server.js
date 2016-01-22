d635f01ba6e8936381317d008bf1cbcbc3006963
neerajindian <coolbuddy90@gmail.com>
2016-01-22 16:14:07 +0530

    Merge branch 'development' of https://github.com/masteroes/SimpleMoney into transactions
    # Conflicts:
    #	server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express=require('express');
 var app=express();
-var tansactionroutes = require('./routes/transaction');
-var login = require('./modules/login/login.js');
+//var login = require('./modules/login/login.js');
 var bodyParser = require('body-parser');
 var parseUrlencoded = bodyParser.urlencoded({extended: false});
 var mongoose = require('mongoose');
@@ -10,7 +9,7 @@ app.get('/',function(req,res){
 	res.send('root url');
 });
 
-var routes = require('./routes/transaction');
+var transaction_routes = require('./routes/transaction');
 var user_routes = require('./routes/users');
 var login_routes = require('./routes/login');
 
@@ -20,7 +19,7 @@ mongoose.connect('mongodb://localhost:27017/simplemoney');
 
 
 //Code to refer to transaction routes
-app.use('/', routes);
+app.use('/', transaction_routes);
 app.use('/', user_routes);
 app.use('/', login_routes);
 
