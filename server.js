b6a60621ea0806f040b2c3d0ee8c6c4f857ac016
neerajindian <coolbuddy90@gmail.com>
2016-01-22 11:57:34 +0530

    added routes for transaction

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express=require('express');
 var app=express();
+var routes = require('./routes/transaction');
 app.use(express.static('public'));
 app.get('/',function(req,res){
 	res.send('root url');
 });
+
+//Code to refer to transaction routes 
+app.use('/', routes);
 
 
 var listener=app.listen(process.env.APP_PORT || 4000,function(){
