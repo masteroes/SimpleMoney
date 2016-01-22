3955bc5254797d3eb5d59b4914b0acc6e3419f6e
ecoworld007@gmail.com <ecoworld007@gmail.com>
2016-01-22 14:25:29 +0530

    done

--- account/account.js
+++ account/account.js
@@ -2,7 +2,9 @@ var shortid = require('shortid');
 var express=require('express');
 var mongoose=require('mongoose');
 var bodyParser = require('body-parser');
-var AccountModel=require('../model/accountModel.js');
+var AccountModel=require('./model/accountModel.js');
+var UserModel=require('./model/users.js');
+var notifyMail=require('./util/util.js');
 var app=express();
 mongoose.connect('mongodb://localhost/mydb');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,6 +28,7 @@ app.get('/users/:userId/accounts/:accountID',function(req,res){
 		res.send(posts);
 	});
 });
+
 //for adding new account of a given user
 app.post('/users/:userId/accounts',function(req,res){
 	if(req.body.userID!=req.params.userId){
@@ -35,11 +38,23 @@ app.post('/users/:userId/accounts',function(req,res){
 		req.body.accountID=shortid.generate();
 		req.body.dateCreated=new Date();
 		var newAccount=new AccountModel(req.body);
-		console.log(newAccount);
+		//console.log(newAccount);
 		newAccount.save(function(err,AccountModel){
 			if(err){
 			res.send("error while creating account for user : "+req.params.userId);
 		}
+			mongoose.model('users').find({username : req.params.userId},function(err,userData){
+		if(err){
+			res.send(req.params.userId+" invalid credential ");
+		}
+			var mymailOpts = {
+			   from: 'loopbacktraining@gmail.com',
+			   to: userData[0].contactinfo.email,
+			   subject: 'Account Creation',
+			   text : 'Hello '+userData[0].username+', \n\n\n A new account id :'+req.body.accountID +' has been created. '
+		}
+	    	notifyMail.sendMail(mymailOpts) 
+		});
 			res.send(AccountModel);
 		});
     }
