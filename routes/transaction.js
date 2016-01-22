8b4e8908c8935920c3ff378ef27199b4ec6b950d
neerajindian <coolbuddy90@gmail.com>
2016-01-22 15:08:29 +0530

    transactions updated

--- routes/transaction.js
+++ routes/transaction.js
@@ -4,21 +4,11 @@ var router = express.Router();
 
 var transaction = require('../models/transaction');
 
-router.get('/transaction', function(req, res) {
-	console.log('Getting transaction details');
-
+router.get('/transactions', function(req, res) {
+	mongoose.model('transactions').find(function(err,transaction){
+	res.send(transaction);	
 });
-
-router.get('/transaction/:txId', function(req, res) {
-	console.log('Getting transaction details for'+ txId);
-
-
 });
-router.get('/transaction/:accId/:type', function(req, res) {
-	
-
-});
-
 router.post('/sendmoney', function(req, res) {
 console.log(req.body);
 	var tx1 = new transaction(req.body);
@@ -28,29 +18,13 @@ console.log(req.body);
 			else
 				res.send(transactions);
 		});
-	
-});
-
-	/*res.send("Just sent money"); 
-	/var date=new Date();
-	date:{type:Date,required: true},
-  	accountHolder:{type:String,required: true},
-  	description: {type:String},
- 	amount:{type:Number,required: true},
-	*/
-	
-
-
-
-	//to aac id, 
-	//from acc id
-	//amount
-	//description
 	//validate amount in account
 	//update from account
 	//create tranc1
 	//create trans 2
+});
 
+	
 module.exports = router;
 
 
