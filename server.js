806597d5ce76283f3cc194d1d1eef1b71882ac03
wmehra <wmehraj@sapient.com>
2016-01-22 14:16:37 +0530

    Integrated Login, Registrations and Profile

--- server.js
+++ server.js
@@ -1,42 +1,20 @@
+var mongoose = require('mongoose');
 var express=require('express');
-var User=require('./userMgmt/models/users.js');
 var app=express();
-var mongoose=require('mongoose');
-var bodyParser = require('body-parser');
-var validator = require("email-validator");
+
+var user_routes = require('./userMgmt/routes/users');
+var login_routes = require('./userMgmt/routes/login');
 
 
-app.use(bodyParser.json());
+// DB Connection
 mongoose.connect('mongodb://localhost:27017/simplemoney');
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.post('/users',function(req,res){
-	if(!validator.validate(req.body.contactinfo.email)){
-		res.send("Email is not valid");
-	}else{
-		var user = new User(req.body);
-		user.save(function(err,users){
-			if(err)
-				res.send(err);
-			else
-				res.send(users);
-		});
-	}
-});
 
 
-app.get('/users',function(req,res){
-	mongoose.model('users').find(function(err,users){
-	res.send(users);
-	});
-});
+//Code to refer to transaction routes
+app.use('/', user_routes);
+app.use('/', login_routes);
 
-app.get('/users/:username',function(req,resp){
-	mongoose.model('users').find({ username: req.params.username },function(err,users){
-	resp.send(users);
-	});
-});
 
-app.listen(process.env.APP_PORT || 4000,function(){
-	//console.log(process.env.APP_PORT)
+listener=app.listen(process.env.APP_PORT || 4000,function(){
+	console.log(process.env.APP_PORT);
 });
